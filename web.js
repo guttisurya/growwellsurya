88461168f99138db5ee26425767a6c453af451af
guttisurya <guttisuryateja@gmail.com>
2022-11-22 19:23:12 +0530

    growwell

---
+++ web.js
@@ -0,0 +1,132 @@
+let aboutButtonEl = document.getElementById("aboutButton");
+let timeToVisitButtonEl = document.getElementById("timeToVisitButton");
+let attractionsButtonEl = document.getElementById("attractionsButton");
+
+let aboutTabEl = document.getElementById("aboutTab");
+let timeToVisitTabEl = document.getElementById("timeToVisitTab");
+let attractionsTabEl = document.getElementById("attractionsTab");
+
+timeToVisitTabEl.classList.add("d-none");
+attractionsTabEl.classList.add("d-none");
+
+function onClickAboutTab() {
+    aboutTabEl.classList.remove("d-none");
+    timeToVisitTabEl.classList.add("d-none");
+    attractionsTabEl.classList.add("d-none");
+
+    aboutButtonEl.classList.add("selected-button");
+    timeToVisitButtonEl.classList.remove("selected-button");
+    attractionsButtonEl.classList.remove("selected-button");
+}
+
+function onClickTimeToVisitTab() {
+    aboutTabEl.classList.add("d-none");
+    timeToVisitTabEl.classList.remove("d-none");
+    attractionsTabEl.classList.add("d-none");
+
+    aboutButtonEl.classList.remove("selected-button");
+    timeToVisitButtonEl.classList.add("selected-button");
+    attractionsButtonEl.classList.remove("selected-button");
+}
+
+function onClickAttractionsTab() {
+    aboutTabEl.classList.add("d-none");
+    timeToVisitTabEl.classList.add("d-none");
+    attractionsTabEl.classList.remove("d-none");
+
+    aboutButtonEl.classList.remove("selected-button");
+    timeToVisitButtonEl.classList.remove("selected-button");
+    attractionsButtonEl.classList.add("selected-button");
+}
+
+
+
+
+
+let aboutButtonEl1 = document.getElementById("aboutButton1");
+let timeToVisitButtonEl1 = document.getElementById("timeToVisitButton1");
+let attractionsButtonEl1 = document.getElementById("attractionsButton1");
+
+let aboutTabEl1 = document.getElementById("aboutTab1");
+let timeToVisitTabEl1 = document.getElementById("timeToVisitTab1");
+let attractionsTabEl1 = document.getElementById("attractionsTab1");
+
+timeToVisitTabEl1.classList.add("d-none");
+attractionsTabEl1.classList.add("d-none");
+
+function onClickAboutTab1() {
+    aboutTabEl1.classList.remove("d-none");
+    timeToVisitTabEl1.classList.add("d-none");
+    attractionsTabEl1.classList.add("d-none");
+
+    aboutButtonEl1.classList.add("selected-button");
+    timeToVisitButtonEl1.classList.remove("selected-button");
+    attractionsButtonEl1.classList.remove("selected-button");
+}
+
+function onClickTimeToVisitTab1() {
+    aboutTabEl1.classList.add("d-none");
+    timeToVisitTabEl1.classList.remove("d-none");
+    attractionsTabEl1.classList.add("d-none");
+
+    aboutButtonEl1.classList.remove("selected-button");
+    timeToVisitButtonEl1.classList.add("selected-button");
+    attractionsButtonEl1.classList.remove("selected-button");
+}
+
+function onClickAttractionsTab1() {
+    aboutTabEl1.classList.add("d-none");
+    timeToVisitTabEl1.classList.add("d-none");
+    attractionsTabEl1.classList.remove("d-none");
+
+    aboutButtonEl1.classList.remove("selected-button");
+    timeToVisitButtonEl1.classList.remove("selected-button");
+    attractionsButtonEl1.classList.add("selected-button");
+}
+
+
+
+
+
+
+
+let aboutButtonEl2 = document.getElementById("aboutButton2");
+let timeToVisitButtonEl2 = document.getElementById("timeToVisitButton2");
+let attractionsButtonEl2 = document.getElementById("attractionsButton2");
+
+let aboutTabEl2 = document.getElementById("aboutTab2");
+let timeToVisitTabEl2 = document.getElementById("timeToVisitTab2");
+let attractionsTabEl2 = document.getElementById("attractionsTab2");
+
+timeToVisitTabEl2.classList.add("d-none");
+attractionsTabEl2.classList.add("d-none");
+
+function onClickAboutTab2() {
+    aboutTabEl2.classList.remove("d-none");
+    timeToVisitTabEl2.classList.add("d-none");
+    attractionsTabEl2.classList.add("d-none");
+
+    aboutButtonEl2.classList.add("selected-button");
+    timeToVisitButtonEl2.classList.remove("selected-button");
+    attractionsButtonEl2.classList.remove("selected-button");
+}
+
+function onClickTimeToVisitTab2() {
+    aboutTabEl2.classList.add("d-none");
+    timeToVisitTabEl2.classList.remove("d-none");
+    attractionsTabEl2.classList.add("d-none");
+
+    aboutButtonEl2.classList.remove("selected-button");
+    timeToVisitButtonEl2.classList.add("selected-button");
+    attractionsButtonEl2.classList.remove("selected-button");
+}
+
+function onClickAttractionsTab2() {
+    aboutTabEl2.classList.add("d-none");
+    timeToVisitTabEl2.classList.add("d-none");
+    attractionsTabEl2.classList.remove("d-none");
+
+    aboutButtonEl2.classList.remove("selected-button");
+    timeToVisitButtonEl2.classList.remove("selected-button");
+    attractionsButtonEl2.classList.add("selected-button");
+}
